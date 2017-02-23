783c812f5693dcb006894eb540c255fc3adfa7af
KRD <kowligideepika18@gmail.com>
2017-02-23 12:57:43 +0530

    First commit

---
+++ db/seeds.rb
@@ -0,0 +1,45 @@
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+
+# courses, books, articles, and the like. Contact us if you are in doubt.
+
+# We make no guarantees that this code is fit for any purpose.
+
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+
+#---
+# 
+#encoding: utf-8
+
+Product.delete_all
+
+Product.create(title: 'Rails, Angular, Postgres, and Bootstrap',
+  
+description:
+   
+ %{<p>
+      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
+      
+As a Rails developer, you care about user experience and performance,
+    
+  but you also want simple and maintainable code. Achieve all that by
+   
+   embracing the full stack of web development, from styling with
+     
+ Bootstrap, building an interactive user interface with AngularJS, 
+to
+      storing data quickly and reliably in PostgreSQL. Take a holistic view of
+     
+ full-stack development to create usable, high-performing applications,
+      
+and learn to use these technologies effectively in a Ruby on Rails
+      environment.
+      </p>},
+
+  image_url: 'dcbang.jpg',   
+ 
+  price: 45.00)
+
